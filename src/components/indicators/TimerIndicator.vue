f9b01dde2a499b5751b0503be4a55e2244f0e8be
Felipe Magagnin <felipebmagagnin@gmail.com>
2025-12-30 18:02:35 -0300

    feat: create timerIndicator component

---
+++ src/components/indicators/TimerIndicator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="bar-container"
+    :style="{ width: props.width, backgroundColor: props.backgroundColor }"
+  >
+    <div
+      class="bar-fill"
+      :style="{
+        width: props.progress + '%',
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  progress: number
+  width?: string
+  backgroundColor?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  width: '100%',
+  backgroundColor: '#333',
+})
+</script>
+
+<style scoped>
+.bar-container {
+  height: 25px;
+  border-radius: 15px;
+  overflow: hidden;
+}
+
+.bar-fill {
+  height: 100%;
+  background: linear-gradient(90deg, #af4c4c, #be3636);
+  transition: width 0.1s;
+}
+</style>
