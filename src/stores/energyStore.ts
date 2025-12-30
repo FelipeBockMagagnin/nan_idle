77ec5e9cec1a379c343b4179f58891ef018c0ed5
Felipe Magagnin <felipebmagagnin@gmail.com>
2025-12-30 18:02:00 -0300

    style: fix code style

--- src/stores/energyStore.ts
+++ src/stores/energyStore.ts
@@ -6,62 +6,63 @@ import Decimal from 'break_infinity.js'
 export const useEnergyStore = defineStore(
   'energy',
   () => {
-  const energy = ref<Energy>({
-    current: new Decimal(0),
-    allocated: new Decimal(0),
-    max: new Decimal(100),
-  })
+    const energy = ref<Energy>({
+      current: new Decimal(0),
+      allocated: new Decimal(0),
+      max: new Decimal(100),
+    })
 
-  function allocateEnergy(value: Decimal): boolean {
-    if (value.greaterThan(getAvailableEnergy())) {
-      return false
+    function allocateEnergy(value: Decimal): boolean {
+      if (value.greaterThan(getAvailableEnergy())) {
+        return false
+      }
+
+      energy.value.allocated = energy.value.allocated.plus(value)
+      return true
     }
 
-    energy.value.allocated = energy.value.allocated.plus(value)
-    return true
-  }
+    function reclaimEnergy(value: Decimal): boolean {
+      if (value.greaterThan(energy.value.allocated)) {
+        return false
+      }
 
-  function reclaimEnergy(value: Decimal): boolean {
-    if (value.greaterThan(energy.value.allocated)) {
-      return false
+      if (energy.value.allocated.subtract(value).lessThan(new Decimal(0))) {
+        return false
+      }
+
+      energy.value.allocated = energy.value.allocated.subtract(value)
+      return true
     }
 
-    if (energy.value.allocated.subtract(value).lessThan(new Decimal(0))) {
-      return false
+    function getAvailableEnergy(): Decimal {
+      return energy.value.current.subtract(energy.value.allocated)
     }
 
-    energy.value.allocated = energy.value.allocated.subtract(value)
-    return true
-  }
+    function regenEnergy(value: Decimal): void {
+      if (energy.value.current.equals(energy.value.max)) return
 
-  function getAvailableEnergy(): Decimal {
-    return energy.value.current.subtract(energy.value.allocated)
-  }
+      energy.value.current = energy.value.current.plus(value)
+    }
 
-  function regenEnergy(value: Decimal): void {
-    if (energy.value.current.equals(energy.value.max)) return
-
-    energy.value.current = energy.value.current.plus(value)
-  }
-
-  return {
-    energy,
-    regenEnergy,
-    allocateEnergy,
-    reclaimEnergy,
-    getAvailableEnergy,
-  }
-},
+    return {
+      energy,
+      regenEnergy,
+      allocateEnergy,
+      reclaimEnergy,
+      getAvailableEnergy,
+    }
+  },
   {
     persist: {
       serializer: {
-        serialize: (state) => JSON.stringify({
-          energy: {
-            current: state.energy.current.toString(),
-            allocated: state.energy.allocated.toString(),
-            max: state.energy.max.toString(),
-          },
-        }),
+        serialize: (state) =>
+          JSON.stringify({
+            energy: {
+              current: state.energy.current.toString(),
+              allocated: state.energy.allocated.toString(),
+              max: state.energy.max.toString(),
+            },
+          }),
         deserialize: (str) => {
           const data = JSON.parse(str)
           return {
