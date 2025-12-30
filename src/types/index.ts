a4cb55dfdcb95f6a8774894fdde3f6a769f95e3a
Felipe Magagnin <felipebmagagnin@gmail.com>
2025-12-30 17:07:53 -0300

    type: create training types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,25 +1,35 @@
 import Decimal from 'break_infinity.js'
 
 // Player-related types
-export interface PlayerStats {
+export type PlayerStats = {
   attack: Decimal
   defence: Decimal
 }
 
 // Energy store types
-export interface Energy {
+export type Energy = {
   current: Decimal
   allocated: Decimal
   max: Decimal
 }
 
 // HP-related types
-export interface HP {
+export type HP = {
   current: Decimal
   max: Decimal
 }
 
 // Enemy types
-export interface Enemy {
+export type Enemy = {
   hp: HP
+}
+
+//Training types
+export type Training = Record<string, TrainingTrait>
+
+export type TrainingTrait = {
+  allocatedEnergy: Decimal
+  level: Decimal
+  trainingSpeed: Decimal
+  progress: Decimal
 }
